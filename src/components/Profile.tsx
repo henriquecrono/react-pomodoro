144b367802dce041b1f78cd28cae8efa2df45afe
Henrique Przybysz <henrique.miranda@virtuspay.com.br>
2021-02-24 21:18:14 -0300

    create Profile component

---
+++ src/components/Profile.tsx
@@ -0,0 +1,13 @@
+const Profile = () => {
+  return (
+    <div>
+      <img src="https://github.com/henriquecrono.png" alt="HEnrique Przybysz" />
+      <div>
+        <strong>Henrique Przybysz</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
