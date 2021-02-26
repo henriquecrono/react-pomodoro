c8ee0ed5f43a896c86786487e531a91efbfdf2af
Henrique Przybysz <henrique.miranda@virtuspay.com.br>
2021-02-26 00:30:41 -0300

    use challenge context at profile component - level

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,15 +1,19 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 const Profile = () => {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/henriquecrono.png" alt="HEnrique Przybysz" />
       <div>
         <strong>Henrique Przybysz</strong>
         <p>
-          <img src="icons/level.svg" alt="Level"/>
-          Level 1
-          </p>
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
+        </p>
       </div>
     </div>
   );
