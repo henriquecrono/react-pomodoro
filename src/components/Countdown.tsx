0ab0e802ddcd17a84286a9ee4d312366d6091d06
Henrique Przybysz <henrique.miranda@virtuspay.com.br>
2021-02-25 22:39:16 -0300

    create button challenged finished

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -4,8 +4,9 @@ import styles from '../styles/components/Countdown.module.css';
 let countdownTimeout: NodeJS.Timeout;
 
 const Countdown = () => {
-  const [time, setTime] = useState(25 * 60);
+  const [time, setTime] = useState(0.1 * 60);
   const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
 
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
@@ -28,6 +29,9 @@ const Countdown = () => {
       countdownTimeout = setTimeout(() => {
         setTime(time - 1);
       }, 1000);
+    } else if (isActive && time == 0) {
+      setHasFinished(true);
+      setIsActive(false);
     }
   }, [isActive, time])
 
@@ -45,25 +49,36 @@ const Countdown = () => {
         </div>
       </div>
 
-      { isActive ? (
+      { hasFinished ? (
         <button
-          type="button"
-          onClick={resetCountdown}
-          className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+          disabled
+          className={styles.countdownButton}
         >
-          Abandonar ciclo
+          Ciclo encerrado
         </button>
       ) : (
-          <button
-            type="button"
-            onClick={startCountdown}
-            className={styles.countdownButton}
-          >
-            Iniciar um ciclo
-          </button>
+          <>
+            { isActive ? (
+              <button
+                type="button"
+                onClick={resetCountdown}
+                className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+              >
+                Abandonar ciclo
+              </button>
+            ) : (
+                <button
+                  type="button"
+                  onClick={startCountdown}
+                  className={styles.countdownButton}
+                >
+                  Iniciar um ciclo
+                </button>
+              )
+            }
+          </>
         )
       }
-
     </div>
   );
 };
