4608fdc1c465864145eaf3408f7a038e21e616d4
Henrique Przybysz <henrique.miranda@virtuspay.com.br>
2021-02-26 00:31:16 -0300

    use challenge context "activeChallenge" at challenge box component

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,10 +1,16 @@
+import { useContext } from 'react';
+
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/ChallengeBox.module.css';
 
+
 const ChallengeBox = () => {
-  const hasActiveChallenge = true;
+  const { activeChallenge } = useContext(ChallengesContext);
+
   return (
     <div className={styles.challengeBoxContainer}>
-      {hasActiveChallenge ? (
+      {activeChallenge ? (
         <div className={styles.challengeActive}>
           <header>Ganhe 400 xp</header>
 
