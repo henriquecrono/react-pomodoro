8f677cba2826ae058dddf5cd814e2a2df75e24f5
Henrique Przybysz <henrique.miranda@virtuspay.com.br>
2021-02-26 00:27:49 -0300

    create challenge interface

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,10 +1,19 @@
 import { createContext, ReactNode, useState } from "react";
 
+import challenges from '../../challenges.json';
+
+
+interface Challenge {
+  body: 'body' | 'eye';
+  description: string;
+  amount: number;
+};
 
 interface ChallengesContextData {
   level: number;
   currentExperience: number;
   challengesCompleted: number;
+  activeChallenge: Challenge;
   levelUp: () => void;
   startNewChallenge: () => void;
 };
@@ -19,13 +28,14 @@ const ChallengesProvider = ({ children }: ChallengesProviderProps) => {
   const [level, setLevel] = useState(1);
   const [currentExperience, setCurrentExperience] = useState(0);
   const [challengesCompleted, setChallengesCompleted] = useState(0);
+  const [activeChallenge, setActiveChallenge] = useState(null);
 
   const levelUp = () => {
     setLevel(level + 1);
   };
 
   const startNewChallenge = () => {
-    console.log('new challenge');
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
   };
 
   return (
@@ -33,6 +43,7 @@ const ChallengesProvider = ({ children }: ChallengesProviderProps) => {
       level,
       currentExperience,
       challengesCompleted,
+      activeChallenge,
       levelUp,
       startNewChallenge
     }}>
