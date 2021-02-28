8aba5b7188ac4805acf4c662078e6ccc9dffd41f
Henrique Przybysz <henrique.miranda@virtuspay.com.br>
2021-02-27 21:22:55 -0300

    using challenge provider at home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,9 @@
-import { ChallengesProvider } from '../contexts/ChallengesContext';
-
 import '../styles/global.css';
 
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <ChallengesProvider>
-      <Component {...pageProps} />
-    </ChallengesProvider>
+    <Component {...pageProps} />
   )
 }
 
